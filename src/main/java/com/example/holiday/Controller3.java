a49a1de9ca4591b4cd1ab896ac53d4a248a06cb7
iasmina012 <iasmina.putina012@yahoo.com>
2022-05-17 20:00:55 +0300

    FIS-22 initial admin gui (fxml, controller)

--- src/main/java/com/example/holiday/Controller3.java
+++ src/main/java/com/example/holiday/Controller3.java
@@ -5,11 +5,5 @@ import javafx.scene.control.Label;
 
 public class Controller3 {
 
-    @FXML
-    Label nameLabel;
-
-    public void displayName (String username) {
-        nameLabel.setText("Hello 3: " + username);
-    }
 }
 
