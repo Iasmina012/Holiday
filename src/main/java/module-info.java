55a2d0050a386c1bd33a2d4993138a8d90c97594
iasmina012 <iasmina.putina012@yahoo.com>
2022-05-20 12:39:16 +0300

    FIS-28 code review

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.holiday {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
     opens com.example.holiday to javafx.fxml;
     exports com.example.holiday;
