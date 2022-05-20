3c8bb4ab797538edad8ce0362d42f888564f9505
Iasmina012 <71723590+Iasmina012@users.noreply.github.com>
2022-05-20 14:53:02 +0300

    Final Product

---
+++ src/main/tests/com/example/holiday/Controller2Test.java
@@ -0,0 +1,38 @@
+package com.example.holiday;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Controller2Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void getDate() {
+    }
+
+    @Test
+    void validButtonAction() {
+    }
+
+    @Test
+    void backButtonAction() {
+    }
+
+    @Test
+    void closeButtonAction() {
+    }
+}
